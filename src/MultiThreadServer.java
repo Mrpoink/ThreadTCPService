9b0efd445aeb331827ff9c3f6c1f767a863b89b4
Brandon Dean <brandondean1068@gmail.com>
2025-03-24 10:38:01 -0400

    Made the change to where the client can send continuous commands.

--- src/MultiThreadServer.java
+++ src/MultiThreadServer.java
@@ -1,15 +1,27 @@
 import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.channels.SocketChannel;
+import java.util.Objects;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class MultiThreadServer {
 
+    public static ServerCommands commands = new ServerCommands();
     public static final String filepath = "SourceFiles/";
+    public static ExecutorService service = Executors.newFixedThreadPool(5);
 
     public static Runnable Wait(SocketChannel socketChannel) {
         try {
+            Start(socketChannel);
+            return null;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void Start(SocketChannel socketChannel) throws IOException {
+        while (true) {
             ByteBuffer buffer = ByteBuffer.allocate(1024);
             int bytesRead = socketChannel.read(buffer);
             System.out.println("Line 24 complete");
@@ -18,19 +30,32 @@ public class MultiThreadServer {
             buffer.get(a);
             String clientMessage = new String(a);
             System.out.println("Line 29: " + clientMessage);
+            switch (clientMessage) {
+                case "L":
+                    commands.List();
+                    break;
+                case "A":
+                    commands.Append();
+                    break;
+                case "U":
+                    commands.Upload();
+                    break;
+                case "D":
+                    commands.Download();
+                    break;
+                case "R":
+                    commands.Rename();
+                    break;
+                case "E":
+                    commands.End();
+                    break;
+            }
             buffer.clear();
-            return null;
-        } catch (Exception e) {
-            throw new RuntimeException(e);
         }
     }
 
     public static void main(String[] args) throws IOException {
-
-        ServerCommands commands = new ServerCommands();
-
-        ExecutorService service = Executors.newFixedThreadPool(5);
-
         service.submit(Wait(commands.socketChannel));
+
     }
 }
